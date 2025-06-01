6d4ed5a73526ec1fbdccfa84b5a085f46004cc7e
Nikolas <etc.nikolas@gmail.com>
2025-06-01 12:08:55 +0200

    Remove user input to fade text

--- textDraw.c
+++ textDraw.c
@@ -4,8 +4,10 @@
 
 #define AMOUNT_TEXTS 9
 #define MAX_R 255
+
 #define TIME_FULL_OPACITY 3.0f
-#define TIME_END 6.0f
+#define TIME_STAY_FULL 6.0f
+#define TIME_END 9.0f
 
 #define BASE_WIDTH 1920.0f
 
@@ -14,7 +16,7 @@
 
 typedef struct TextEntity {
     char *text;
-    /// Multiplier for screen size
+    /// Multiplier for screen position
     Vector2 pos;
 } TextEntity;
 
@@ -22,7 +24,6 @@ typedef struct TextDraw {
     Font font;
     Color color;
     float timer;
-    bool userInputRegistered;
     int currentLevel;
     TextEntity texts[AMOUNT_TEXTS];
 } TextDraw;
@@ -30,8 +31,8 @@ typedef struct TextDraw {
 static void setPos(Vector2 *pos, int lvl) {
     switch (lvl) {
     case 1:
-		pos->x = 0.05f;
-		pos->y = 0.05f;
+        pos->x = 0.05f;
+        pos->y = 0.05f;
         break;
     }
 }
@@ -40,7 +41,6 @@ TextHandle text_init() {
     TextHandle text = malloc(sizeof(TextDraw));
     text->font = LoadFont("assets/pixelplay.png");
     text->timer = TIME_END + 1.0f;
-    text->userInputRegistered = false;
     text->currentLevel = 1;
 
     text->color.r = DARKPURPLE.r;
@@ -50,9 +50,9 @@ TextHandle text_init() {
 
     text->texts[LVL_INDX(1)].text = "For three days,\nI wander these depths";
 
-	for (int i = 0; i < AMOUNT_TEXTS; i++) {
-		setPos(&(text->texts[i].pos), i+1);
-	}
+    for (int i = 0; i < AMOUNT_TEXTS; i++) {
+        setPos(&(text->texts[i].pos), i + 1);
+    }
 
     return text;
 }
@@ -65,15 +65,23 @@ void text_activateLevelText(TextHandle handle, int level) {
     handle->currentLevel = level;
     handle->timer = 0.0f;
     handle->color.a = 0;
-    handle->userInputRegistered = false;
 }
 
+/*
+ * Build up opacity until TIME_FULL_OPACITY
+ * Stay at full opacity until TIME_STAY_FULL
+ * Loose opacity until TIME_END
+ */
 static unsigned char getOpacity(float timer) {
     unsigned int opa;
     if (timer < TIME_FULL_OPACITY) {
         opa = (unsigned int)(timer * MAX_R / TIME_FULL_OPACITY);
+    } else if (timer > TIME_STAY_FULL) {
+        opa = (unsigned int)(((TIME_END - TIME_STAY_FULL) -
+                              (timer - TIME_STAY_FULL)) *
+                             MAX_R / (TIME_END - TIME_STAY_FULL));
     } else {
-        opa = (unsigned int)(((TIME_END - TIME_FULL_OPACITY) - (timer - TIME_FULL_OPACITY)) * MAX_R / (TIME_END - TIME_FULL_OPACITY));
+        opa = MAX_R;
     }
 
     return opa > MAX_R ? MAX_R : (unsigned char)opa;
@@ -82,14 +90,8 @@ static unsigned char getOpacity(float timer) {
 void text_update(TextHandle handle) {
     if (handle->timer > TIME_END)
         return;
-    if ((handle->timer > TIME_FULL_OPACITY && handle->userInputRegistered) ||
-        handle->timer <= TIME_FULL_OPACITY) {
-        handle->timer += GetFrameTime();
-    } else if (handle->timer > TIME_FULL_OPACITY) {
-        if (IsKeyPressed(KEY_ENTER)) {
-            handle->userInputRegistered = true;
-        }
-    }
+
+    handle->timer += GetFrameTime();
 
     handle->color.a = getOpacity(handle->timer);
 }
