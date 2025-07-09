03f6acc93a5b60b4704c0a81e8b357a728f3c74a
Nikolas <etc.nikolas@gmail.com>
2025-07-09 20:07:38 +0200

    Fade particle over time

--- diamondParticles.c
+++ diamondParticles.c
@@ -3,14 +3,18 @@
 #include "include/raylib/raymath.h"
 #include <stdlib.h>
 
-#define NUMBER_OF_PARTICLES 30
-#define SPAWN_DELAY_MAX 1.5f
-#define PARTICLE_SPEED 0.3f
+#define NUMBER_OF_PARTICLES 50
+#define SPAWN_DELAY_MAX 0.8f
+#define PARTICLE_SPEED 0.5f
 
 #define MAX_OFFSET_X 10
 #define MAX_OFFSET_Y 25
 
-#define PURPLE_OPAQUE     CLITERAL(Color){ 200, 122, 255, 100 }   // Purple
+#define MIN_PARTICLE_SIZE 200
+#define MAX_PARTICLE_SIZE 400
+
+#define PURPLE_OPAQUE \
+    CLITERAL(Color) { 200, 122, 255, 200 } // Purple
 
 typedef enum Direction {
     UP,
@@ -19,10 +23,12 @@ typedef enum Direction {
 
 typedef struct DiamondParticle {
     Vector2 pos;
-	Vector2 targetOffset;
+    Vector2 targetOffset;
     Direction dir;
     float startLife;
-	bool active;
+    Color color;
+    float size;
+    bool active;
 } DiaParticle;
 
 ParticleHandler diap_init(int xStart, int yUpStart, int yDownStart) {
@@ -35,10 +41,12 @@ ParticleHandler diap_init(int xStart, int yUpStart, int yDownStart) {
         particles[i].pos.x = (float)xStart;
         particles[i].pos.y = (float)GetRandomValue(yUpStart, yDownStart);
         particles[i].dir = GetRandomValue(0, 1) == 0 ? UP : DOWN;
-		particles[i].active = false;
+        particles[i].active = false;
 
-		particles[i].targetOffset.x = (float)GetRandomValue(MAX_OFFSET_X * -1, MAX_OFFSET_X);
-		particles[i].targetOffset.y = (float)GetRandomValue(MAX_OFFSET_Y * -1, MAX_OFFSET_Y);
+        particles[i].targetOffset.x = (float)GetRandomValue(MAX_OFFSET_X * -1, MAX_OFFSET_X);
+        particles[i].targetOffset.y = (float)GetRandomValue(MAX_OFFSET_Y * -1, MAX_OFFSET_Y);
+        particles[i].color = PURPLE_OPAQUE;
+        particles[i].size = (float)GetRandomValue(MIN_PARTICLE_SIZE, MAX_PARTICLE_SIZE) / 100.0f;
     }
     return particles;
 }
@@ -48,21 +56,28 @@ void diap_free(ParticleHandler handler) {
 }
 
 void diap_update(ParticleHandler handler, float lifetime, int diaPosX, int diaPosY) {
-	Vector2 diamond = { (float)diaPosX, (float)diaPosY };
-	Vector2 diamondOffset;
-	for (int i = 0; i < NUMBER_OF_PARTICLES; i++) {
-		if (handler[i].startLife <= lifetime) {
-			handler[i].active = true;
-			diamondOffset = Vector2Add(diamond, handler[i].targetOffset);
-			handler[i].pos = Vector2MoveTowards(handler[i].pos, diamondOffset, PARTICLE_SPEED);
-		}
-	}
+    Vector2 diamond = {(float)diaPosX, (float)diaPosY};
+    Vector2 diamondOffset;
+    for (int i = 0; i < NUMBER_OF_PARTICLES; i++) {
+        if (handler[i].startLife <= lifetime) {
+            handler[i].active = true;
+            diamondOffset = Vector2Add(diamond, handler[i].targetOffset);
+
+            // Stayed in same x-place since last frame
+            if (diamondOffset.x == handler[i].pos.x) {
+                if (handler[i].color.a != 0) {
+                    handler[i].color.a--;
+                }
+            }
+            handler[i].pos = Vector2MoveTowards(handler[i].pos, diamondOffset, PARTICLE_SPEED);
+        }
+    }
 }
 
 void diap_drawParticles(ParticleHandler handler) {
-	for (int i = 0; i < NUMBER_OF_PARTICLES; i++) {
-		if (handler[i].active) {
-			DrawCircle((int)handler[i].pos.x, (int)handler[i].pos.y, 4, PURPLE_OPAQUE);
-		}
-	}
+    for (int i = 0; i < NUMBER_OF_PARTICLES; i++) {
+        if (handler[i].active) {
+            DrawCircle((int)handler[i].pos.x, (int)handler[i].pos.y, handler[i].size, handler[i].color);
+        }
+    }
 }
